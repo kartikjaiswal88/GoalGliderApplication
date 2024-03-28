8886c304b161413c28607080256ff1bbb5193ed3
Kartik Jaiswal <kartikjaiswal038@gmail.com>
2024-03-28 19:47:02 +0530

    Better UI of Register.html and login.html

--- GoalGliderApplication/src/main/java/com/example/registrationlogindemo/config/SpringSecurity.java
+++ GoalGliderApplication/src/main/java/com/example/registrationlogindemo/config/SpringSecurity.java
@@ -28,7 +28,7 @@ public class SpringSecurity {
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http.csrf().disable()
-				.authorizeHttpRequests((authorize) -> authorize.requestMatchers("/register/**","/email/**","/forget-password/**","/verify-forgot-otp/**","reset-password/**","/images/Logo2.png","/images/Logo.png").permitAll()
+				.authorizeHttpRequests((authorize) -> authorize.requestMatchers("/register/**","/email/**","/forget-password/**","/verify-forgot-otp/**","reset-password/**","/images/Logo2.png","/images/Logo.png","/favicon.ico").permitAll()
 						 .requestMatchers("/welcome","/listtodos","/add-todo","/delete-todo","/update-todo","/mark-as-done","/pdf/createPdf","/pieChart","/start-stopwatch/{todoId}").hasRole("ADMIN"))
 				.formLogin(form -> form.loginPage("/login").loginProcessingUrl("/login").defaultSuccessUrl("/welcome")
 						.permitAll())
